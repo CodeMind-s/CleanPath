6dd7ff31c07079cacd4e4026a53559f452fe8372
Muhammed Arshaq <mjmarshaq@gmail.com>
2024-10-16 22:55:39 +0530

    Refactor transactionApi.js to add new functions for fetching transactions by user ID and updating transactions

--- frontend/src/api/transactionApi.js
+++ frontend/src/api/transactionApi.js
@@ -1,5 +1,14 @@
 import ApiHelper from "../helpers/apiHelper";
 
+/**
+ * Creates a new transaction.
+ *
+ * @async
+ * @function createTransaction
+ * @param {Object} transaction - The transaction data to create.
+ * @returns {Promise<Object>} A promise that resolves to the created transaction object.
+ * @throws Will throw an error if the creation operation fails.
+ */
 const createTransaction = async (transaction) => {
   try {
     const createdTransaction = await new ApiHelper().post(
@@ -13,6 +22,14 @@ const createTransaction = async (transaction) => {
   }
 };
 
+/**
+ * Fetches all transactions.
+ *
+ * @async
+ * @function getAllTransactions
+ * @returns {Promise<Object[]>} A promise that resolves to an array of transaction objects.
+ * @throws Will throw an error if the fetch operation fails.
+ */
 const getAllTransactions = async () => {
   try {
     const transactions = await new ApiHelper().get("transactions", {});
@@ -23,6 +40,14 @@ const getAllTransactions = async () => {
   }
 };
 
+/**
+ * Fetches transactions for the current user.
+ *
+ * @async
+ * @function getUserTransactions
+ * @returns {Promise<Object[]>} A promise that resolves to an array of transaction objects.
+ * @throws Will throw an error if the fetch operation fails.
+ */
 const getUserTransactions = async () => {
   try {
     const userTransactions = await new ApiHelper().get(`transactions/user`, {});
@@ -33,6 +58,15 @@ const getUserTransactions = async () => {
   }
 };
 
+/**
+ * Fetches a transaction by its ID.
+ *
+ * @async
+ * @function getTransactionById
+ * @param {string} id - The ID of the transaction to fetch.
+ * @returns {Promise<Object>} A promise that resolves to the transaction object.
+ * @throws Will throw an error if the fetch operation fails.
+ */
 const getTransactionById = async (id) => {
   try {
     const transaction = await new ApiHelper().get(`transactions/${id}`, {});
@@ -43,6 +77,37 @@ const getTransactionById = async (id) => {
   }
 };
 
+/**
+ * Fetches transactions for a given user ID.
+ *
+ * @async
+ * @function getTransactionsByUserId
+ * @param {string} userId - The ID of the user whose transactions are to be fetched.
+ * @returns {Promise<Object[]>} A promise that resolves to an array of transaction objects.
+ * @throws Will throw an error if the fetch operation fails.
+ */
+const getTransactionsByUserId = async (userId) => {
+  try {
+    const transactions = await new ApiHelper().get(
+      `transactions/${userId}`,
+      {}
+    );
+    return transactions;
+  } catch (error) {
+    console.error("Error fetching transactions by user ID:", error.message);
+    throw error; // Rethrow the error for the component to handle
+  }
+};
+
+/**
+ * Updates a transaction to mark it as paid.
+ *
+ * @async
+ * @function updateTransaction
+ * @param {string} id - The ID of the transaction to update.
+ * @returns {Promise<Object>} The updated transaction object.
+ * @throws Will throw an error if the update operation fails.
+ */
 const updateTransaction = async (id) => {
   try {
     const updatedTransaction = await new ApiHelper().put(`transactions/${id}`, {
@@ -60,5 +125,6 @@ export {
   getAllTransactions,
   getUserTransactions,
   getTransactionById,
+  getTransactionsByUserId,
   updateTransaction,
 };
